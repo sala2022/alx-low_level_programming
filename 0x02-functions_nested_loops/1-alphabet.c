e26fae9a8af78a1c9a7d965138b25cdcfb8ec3bf
sala2022 <saloua.kasmibakkali@gmail.com>
2023-03-21 16:14:31 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,15 @@
 
 	/**
 	 * print_alphabet - print all alphabet in lowercase
+	 * is a function that outputs the alphabet in some form
+	 * It may take different parameters or have different variations,
+	 * but in general, it could be expected to output the 26 letters
+	 * of the English alphabet
 	 */
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
