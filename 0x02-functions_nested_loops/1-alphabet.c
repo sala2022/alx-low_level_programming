d28a20a90c8021144baebbbb28cfe8bb1a6c20a9
sala2022 <saloua.kasmibakkali@gmail.com>
2023-03-21 16:29:08 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 
-	/** Prints the alphabet in lowercase
+	/** print_alphabetis a function that outputs the alphabet
+	 * in some form.It may take different parameters or have different
+	 * variations, but in general, it could be expected to output
+	 *the 26 letters of the English alphabet
 	 */
 void print_alphabet(void)
 {
