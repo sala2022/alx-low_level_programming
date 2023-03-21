1d93f4589596ac02bfb98592ed1190489aa89b6f
sala2022 <saloua.kasmibakkali@gmail.com>
2023-03-21 15:02:33 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,11 @@
 #include "main.h"
-
 /**
-
-*main - Entry point
-
-*This function is the entry point of the program. It is called when the
-
-*program is started and is responsible for initializing the program and
-
-*calling other functions that perform the program's main tasks.
-
-*Return: Always 0 (Success)
-*/
-int main(void)
+	 * main - print _putchar
+	 * This function is the entry point of the program. It is called when the
+	 * program is started and is responsible for initializing the program and calling other functions that perform the program's main tasks.
+	 * Return: Always 0 (Success)
+	 */
+int main (void)
 {
 	_putchar('_');
 	_putchar('p');
