b9919a29a84c389751c1b096764eac68214b5e23
sala2022 <saloua.kasmibakkali@gmail.com>
2023-03-21 17:09:32 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,19 +9,18 @@ void print_alphabet_x10(void)
 {
 	 char ch;
 	 int i;
+
 	 i=0;
 
 
-         while (i < 10)
-	 {
-		 ch = 'a';
-		 while (ch <= 'z')
-		 {
-			 _putchar(ch);
-			 ch++;
-		 }
-		 _putchar('\n');
-		 i++;
-	 }
-}
-
+	while (i < 10)
+	{
+		ch = 'a';
+	       while (ch <= 'z')
+	       {
+		       _putchar(ch);
+		       ch++;
+	       }
+	       _putchar('\n');
+	       i++;
+	}
