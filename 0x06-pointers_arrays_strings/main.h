7fa7b2d8f6db6a713827cd5f6b57a8c2d40fb6dc
sala2022 <saloua.kasmibakkali@gmail.com>
2023-03-29 14:30:25 -0700

    updated files

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,6 +10,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-char *_strstr(char *haystack, char *needle);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
